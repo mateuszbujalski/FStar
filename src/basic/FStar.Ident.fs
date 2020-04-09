c0b36cc4864e23e102ac82113eea44b7736f4d06
Guido Martínez <mtzguido@gmail.com>
2020-04-09 01:03:31 -0300

    FStar.Ident: hash ids and lids for fast equality comparison

--- src/basic/FStar.Ident.fs
+++ src/basic/FStar.Ident.fs
@@ -3,9 +3,11 @@ module FStar.Ident
 
 open Prims
 open FStar.Range
+open FStar.Util
 
 // IN F*: [@ PpxDerivingYoJson PpxDerivingShow ]
 type ident = {idText:string;
+              text_hash:hash_val;
               idRange:Range.range}
 
 // IN F*: [@ PpxDerivingYoJson PpxDerivingShow ]
@@ -18,12 +20,13 @@ type ipath = list<ident>
 type lident = {ns:ipath; //["FStar"; "Basic"]
                ident:ident;    //"lident"
                nsstr:string; // Cached version of the namespace
+               str_hash:hash_val; // a hash of 'str', for fast comparison
                str:string} // Cached version of string_of_lid
 
 // IN F*: [@ PpxDerivingYoJson PpxDerivingShow ]
 type lid = lident
 
-let mk_ident (text,range) = {idText=text; idRange=range}
+let mk_ident (text,range) = {idText=text; text_hash=phys_hash text; idRange=range}
 
 let set_id_range r i = { i with idRange =r }
 
@@ -56,10 +59,12 @@ let ns_of_lid lid = lid.ns
 let ids_of_lid lid = lid.ns@[lid.ident]
 let lid_of_ns_and_id ns id =
     let nsstr = List.map text_of_id ns |> text_of_path in
+    let s = (if nsstr="" then id.idText else nsstr ^ "." ^ id.idText) in
     {ns=ns;
      ident=id;
      nsstr=nsstr;
-     str=(if nsstr="" then id.idText else nsstr ^ "." ^ id.idText)}
+     str_hash=phys_hash s;
+     str=s}
 let lid_of_ids ids =
     let ns, id = Util.prefix ids in
     lid_of_ns_and_id ns id
@@ -69,8 +74,15 @@ let lid_of_path path pos =
     let ids = List.map (fun s -> mk_ident(s, pos)) path in
     lid_of_ids ids
 let text_of_lid lid = lid.str
-let lid_equals l1 l2 = l1.str = l2.str
-let ident_equals id1 id2 = id1.idText = id2.idText
+
+let lid_equals l1 l2 =
+    l1.str_hash = l2.str_hash
+    && l1.str = l2.str
+
+let ident_equals id1 id2 =
+    id1.text_hash = id2.text_hash
+    && id1.idText = id2.idText
+
 let range_of_lid (lid:lid) = range_of_id lid.ident
 let set_lid_range l r = {l with ident={l.ident with idRange=r}}
 let lid_add_suffix l s =
